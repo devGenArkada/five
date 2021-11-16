1c5b78c78968660675c3b6c44298800b03f256a0
devGenArkada <dev_gen@arkada-web.ru>
2021-11-16 17:51:26 +0300

    sliderFunctionality

--- project/assets/frontend/js/main.js
+++ project/assets/frontend/js/main.js
@@ -31,32 +31,66 @@ window.onload = function() {
   }
 
   function chechTestimonial (thisBlock) {
-    let thisBlockText = thisBlock.find('.testimonialsSliderItem__text')
+    var thisBlockText = thisBlock.find('.testimonialsSliderItem__text')
     if (thisBlockText.prop('scrollHeight') > $(".testimonialsSliderItem__text").height() ) {
       //this element is overflowing on the y axis
-      thisBlock.addClass('test')
+      thisBlock.addClass('testimonialsSliderItem--scrolled')
       console.log('overflowen')
     } else {
       console.log('no-overflow')
     }
   }
 
-  function testimonials (){
-    $('.testimonialsSliderItem').each(function() {
-      let thisBlock = $( this );
-      chechTestimonial(thisBlock);
+  function showMore () {
+    $('.testimonialsSliderItem__moreBtn').on('click', function(){
+      console.log('show more');
+      $(this).closest('.testimonialsSliderItem').toggleClass('testimonialsSliderItem--full');
+      console.log($(this).closest('.testimonialsSliderItem'))
     });
   }
 
+  function testimonials (){
+    $('.testimonialsSliderItem').each(function() {
+      var thisBlock = $( this );
+      chechTestimonial(thisBlock);
+    });
+    showMore();
+  }
+
+
+  function testimonialsSlider() {
+    $('.testimonialsSlider').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1
+    });
+  }
+
+  function countVisibleCharacters(element) {
+    var text = $('.testimonialsSliderItem__text');
+    var r = 0;
+    element.removeChild(element.firstChild);
+    for(var i = 0; i < text.length; i++) {
+        var newNode = document.createElement('span');
+        newNode.appendChild(document.createTextNode(text.charAt(i)));
+        element.appendChild(newNode);
+
+        if(newNode.offsetLeft < element.offsetWidth) {
+            r++;
+        }
+    }
+    return r;
+  }
 
   
 
 
 
   // mobileMenu();
+  testimonialsSlider();
   productWishlist();
   // testimonials();
   testimonials();
+  // countVisibleCharacters();
   
   // BusinessCardDecor();
 
